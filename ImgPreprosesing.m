75b06afbdab99e5b80498d739a15ddc8b42901c0
ramezemad <75076742+ramezemad@users.noreply.github.com>
2022-05-19 22:47:20 +0200

    Update ImgPreprosesing.m

--- ImgPreprosesing.m
+++ ImgPreprosesing.m
@@ -1,18 +1,24 @@
-Img=imread('DB\12_2.jpg');
+Img=imread('DB\first_session\12_1.jpg');
 %Input image
-%figure,imshow(Img); axis off; title('Original Image'); hold off;
+figure,imshow(Img); axis off; title('Original Image'); hold off;
 
 %Adjust Mean,Variance
 %Img=rgb2gray(Img);
-%NormalizedFP = Normalization(Img,100,10000);
+%NormalizedFP = Normalization(Img,100,10000);           % seems to just turn the background greyer: TEST ACCURACY
 %figure, imshow(NormalizedFP); axis off; title('Task 1 Normalization'); hold off;
 
-Sharpened = imsharpen(Img,'Radius', 1.5,'Amount',1.2); %
+Sharpened = imsharpen(Img,'Radius', 1.5,'Amount',1.2);
 figure; imshow(Sharpened, []); axis off; title('Sharpened'); hold off;
 
 HistFP = histeq(Sharpened);        % not used yet
 %figure,imshow(HistFP); axis off; title('Histogram Equalization'); hold off;
+%Sharpened = HistFP;                % testing to use the histFP, but seems to cause worse results.: TEST ACCURACY
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Binarizing Image                    % Try it before the Fourrier TESTINGGG: TEST ACCURACY
+% Threshold=graythresh(Img);
+% BinaryImg=imbinarize(Img,Threshold);
+% Sharpened = BinaryImg;
 
 hsize = 7;
 hsigma = 0.2;
@@ -23,14 +29,13 @@ J = fftshift(fft2(double(Sharpened)));
 Jh = imfilter(J,h);
 Jh = J - Jh;
 Fourierred = abs(ifft2(ifftshift(Jh)));
-
 figure; imshow(Fourierred, []); axis off; title('After FFT'); hold off;     % should be affecting the frequency
 
-%SegmentedFP = Fourierred;
 [SegmentedFP, mask] = ridgesegment(Fourierred,16,0.1); %3ando gabor
-figure,imshow(SegmentedFP); axis off; title('Segmentation'); hold off;% seems to be just like the after fft
+figure,imshow(SegmentedFP); axis off; title('Segmentation'); hold off;      % seems to be just like the after fft
+%SegmentedFP = Fourierred;   %               TESTING: TEST ACCURACY
 
-%Binarizing Image                    : Try it before the Fourrier 
+%Binarizing Image                    : Try it before the Fourrier : TEST ACCURACY
  %Threshold=graythresh(Img);
  %BinaryImg=imbinarize(Img,Threshold);
  %figure,imshow(BinaryImg);
@@ -39,15 +44,14 @@ figure,imshow(SegmentedFP); axis off; title('Segmentation'); hold off;% seems to
 DMask  = double(Direction) .*double(mask);
 [x,y,u,v] = directionmap(DMask, 7, SegmentedFP);
 CMask = double(Coherence).*double(mask);
-figure; imshow(SegmentedFP,[]);  axis off; title('Directions'); hold on;
-quiver(x,y,u,v,0,'.','linewidth',1); hold off;
+%figure; imshow(SegmentedFP,[]);  axis off; title('Directions'); hold on;
+%quiver(x,y,u,v,0,'.','linewidth',1); hold off;
 
 [freq, medianfreq,waveLength] = ridgefreq(SegmentedFP, mask, Direction, 28, 5 ,3 , 15);
 figure,imshow(freq); axis off; title('Frequency Matrix'); hold off;
 
-newim = ridgefilter(SegmentedFP, Direction, freq, 0.5, 0.5);
-
-figure,imshow(newim); axis off; title('Final Image'); hold off;
+EnhancedImg = ridgefilter(SegmentedFP, Direction, freq, 0.5, 0.5);
+figure,imshow(EnhancedImg); axis off; title('Final Image'); hold off;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -66,21 +70,25 @@ figure,imshow(newim); axis off; title('Final Image'); hold off;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Feature Extraction
 
-[seg, mask2] = ridgesegment(newim,16,0.1); %3ando gabor
+[seg, mask2] = ridgesegment(EnhancedImg,16,0.1); %3ando gabor
+Threshold=graythresh(EnhancedImg);
+BinaryImg=imbinarize(EnhancedImg,Threshold);
+%figure,imshow(BinaryImg);
 
-Threshold=graythresh(newim);
- BinaryImg=imbinarize(newim,Threshold);
- %figure,imshow(BinaryImg);
-
- 
 thinned  = ~bwmorph(~BinaryImg,'thin',Inf); % 'skel'
-skeleton =  bwmorph(thinned,'spur',20);
+spured =  bwmorph(thinned,'spur',20);
 binarisedMask = BinaryImg.*mask2;
-skeletonMask = skeleton.*mask2;
+skeleton = spured.*mask2;
+figure; imshow(skeleton,[]); axis off; title('Skeleton'); hold off;
 
-%figure; imshow(skeletonMask,[]); axis off; title('Skeleton'); hold off;
 
-[Gx2, Gy2, D2, C2] = ridgeorientation(newim, 1, 17, 3);
+
+
+
+
+
+
+[Gx2, Gy2, D2, C2] = ridgeorientation(EnhancedImg, 1, 17, 3);
 C2Mask = double(C2).*double(mask2);
 minima = ~imregionalmin(C2Mask);
 candidateFeatures = double(~minima).*double(mask2);
